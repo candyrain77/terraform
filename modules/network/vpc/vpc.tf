ea2f5e24bca062d1e326d0229d13ede3f9255bf5
candyrain77 <37399904+candyrain77@users.noreply.github.com>
2019-07-22 23:15:54 +0200

    Update vpc.tf

--- modules/network/vpc/vpc.tf
+++ modules/network/vpc/vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "DefaultVpc" {
   cidr_block = "${var.vpc_cidr_block}"
   enable_dns_hostnames = "${var.vpc_booleantrue}"
   enable_dns_support = "${var.vpc_booleantrue}"
-  tags {
+  tags ={
     Name        = "Vpc ${var.tag_vpc_name}"
     Region = "${var.tag_vpc_region}"
   }
